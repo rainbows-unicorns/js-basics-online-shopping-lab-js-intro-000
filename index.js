223d0d42c544679710c57156cfc6c20bbbeb1a01
maureen mclean <maureenhm@gmail.com>
2017-01-30 20:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
-var cart;
+var cart = [];
 
-function setCart(newCart) {
+function setCart(newCart)
+{
   cart = newCart;
 }
+
+function getCart()
+{
+  return cart;
+}
+
+function addToCart(item)
+{
+  
+  var price = Math.floor(Math.random() * 101);
+
+  cart.push({item:price});
+  console.log(`${item} has been added to cart.`);
+  return cart;
+}
+
+function viewCart()
+{
+  if (cart == null)
+  {
+    console.log(`Your shopping cart is empty.`)
+  }
+  else
+  {
+    for (var i = 0; i < cart.length; i++)
+    {
+      for (var item in cart[i])
+      {
+      console.log(`In your cart, you have ${[item:price].}`)
+      }
+    }
+  }
+  return cart;
+}
+
+function removeFromCart(item)
+{
+
+  {
+    if ({item:price}] === null;
+      console.log(`That item is not in your cart.`);
+    else
+    {
+      cart.pop({item:price});
+      console.log(`${item} has been added to cart.`);
+    }
+  }
+  return cart;
+}
+
 
 function total() {
   let t = 0
